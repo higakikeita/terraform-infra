267f727af3322e3b08b36d0d612411093d392bad
keitahigaki <higaki.keichan@gmail.com>
2025-06-17 06:55:54 +0900

    Add initial dev/stg/prd environment with main.tf

---
+++ stg/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  backend "remote" {
+    organization = "higakikeita-org"  # Terraform CloudのOrganization名に変更
+    workspaces {
+      name = "example-stg"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket        = "example-bucket-${terraform.workspace}"
+  force_destroy = true
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.example.id
+}
+
