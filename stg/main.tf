812f91cb8d20794151bc540654a3ca392a692b8a
keitahigaki <higaki.keichan@gmail.com>
2025-06-18 12:09:10 +0900

    srg tf-create

--- stg/main.tf
+++ stg/main.tf
@@ -1,8 +1,6 @@
 terraform {
   required_version = ">= 1.12.2"
-
-  }
-
+}
 
 provider "aws" {
   region = "ap-northeast-1"
@@ -11,8 +9,10 @@ provider "aws" {
 resource "aws_s3_bucket" "example" {
   bucket        = "tfc-demo-${var.env}"
   force_destroy = true
+
   tags = {
     Environment = var.env
   }
-}}
+}
+
 
